7b73ae1c6bdc3272dc982f21e1899d2a2a8f8a43
Hairismahoreee <hairismahoree@gmail.com>
2023-08-23 16:18:18 +0800

    correction version

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,10 @@ import { AppModule } from "./app.module"
 import { ValidationPipe } from "@nestjs/common"
 
 async function bootstrap() {
-<<<<<<< HEAD
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
-  await app.listen(3000);
-
-=======
     const app = await NestFactory.create(AppModule)
     app.enableCors()
     app.useGlobalPipes(new ValidationPipe())
     await app.listen(3000)
->>>>>>> b339f12a8622bdda714ca86a4edf955a9be849d3
 }
 
 bootstrap()
